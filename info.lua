42604d6c9d1c50d4f6076e7f081d9216c1fe67a4
Bruno Dechen <45558134+d3ch3n@users.noreply.github.com>
2026-07-21 18:52:41 -0300

    Bump build version to 1.0.3.0

--- info.lua
+++ info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
   Name = "DCH~ELO~Elo IDS Monitor",
   Version = "0.0",
-  BuildVersion = "1.0.2.0",
+  BuildVersion = "1.0.3.0",
   Id = "55cb53bb-7043-4097-bfb1-d7fb12e66d00",
   Author = "DCH",
   Description = "Elo IDS monitor MDC control plugin"
